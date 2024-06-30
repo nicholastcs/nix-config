bc7052b1c45a660a5df98e48cda4001b8ad65cec
nicholastcs <nictan11@gmail.com>
2024-07-01 00:41:45 +0800

    chore: init repo

---
+++ development-env.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, ... }:
+
+{
+  virtualisation.docker.enable = true;
+
+  users.users.nicholastcs.extraGroups = [ "docker" ];
+
+  environment.sessionVariables = rec {
+    NIXOS_OZONE_WL = "1";
+    KUBE_EDITOR = "nano";
+  };
+
+  networking.extraHosts = 
+  ''
+    192.168.49.2 test-ingress.org
+  '';
+
+  users.users.nicholastcs.packages = with pkgs; [
+    # For development
+    go_1_21
+    git
+
+    # infrastructure
+    minikube
+    kubectl
+    kubernetes-helm
+    mkcert
+
+    (vscode-with-extensions.override {
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.nix
+        golang.go
+        hashicorp.terraform
+        pkief.material-icon-theme
+        redhat.vscode-yaml
+        ms-kubernetes-tools.vscode-kubernetes-tools
+      ];
+    })
+
+    # IAC
+    tenv
+  ];
+}
